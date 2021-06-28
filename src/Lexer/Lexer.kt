d5d3b8a69890125a2cf52c8ea5aab32eb722dc8c
Testmerry <Testmerry@example.com>
2021-06-28 23:24:04 +0300

    Initial commit

--- src/Lexer/Lexer.kt
+++ src/Lexer/Lexer.kt
@@ -5,29 +5,29 @@ class Lexer {
         fun lex(input: String): LinkedList<Token> {
             val tokens = LinkedList<Token>()
             var istype: Boolean
-            var lexeme: Lexeme? = null //текущее значение type
-            var substr = "" //текущее значение data
-            var i = 0 //индекс перемещения по строке
+            var lexeme: Lexeme? = null
+            var substr = ""
+            var i = 0
             while (input.length > i) {
                 istype = false
-                substr += input[i] //считываем следующий символ
-                for (l in Lexeme.values()) { //определяем тип токена
+                substr += input[i]
+                for (l in Lexeme.values()) {
                     val pattern = l.pattern
                     val matcher = pattern.matcher(substr)
                     if (matcher.matches()) {
                         lexeme = l
-                        istype = true //если определили успешно, то изменяем текущее значение токена и идем дальше
+                        istype = true
                         break
                     }
                 }
-                if (input[i] == ' ' || input[i] == '\n' || input[i] == '\t') { //если пробел, перенос строки или табуляция, то игнорируем
+                if (input[i] == ' ' || input[i] == '\n' || input[i] == '\t') {
                     if (lexeme != null) {
                         tokens.add(Token(lexeme, substr.substring(0, substr.length - 1)))
                         lexeme = null
                         substr = ""
                     }
                 }
-                if (lexeme != null && !istype) { //если тип токена не определился при считывании нового символа, то возвращаемся назад на 1 символ и добавляем токен
+                if (lexeme != null && !istype) {
                     substr = substr.substring(0, substr.length - 1)
                     i--
                     tokens.add(Token(lexeme, substr))
@@ -36,7 +36,7 @@ class Lexer {
                 }
                 i++
             }
-            if (lexeme != null) tokens.add(Token(lexeme, substr)) //добавляем последний токен
+            if (lexeme != null) tokens.add(Token(lexeme, substr))
             return tokens
         }
     }
--- src/Main.kt
+++ src/Main.kt
@@ -2,13 +2,11 @@
 import Lexer.Lexer.Companion.lex
 
 import Parser.Parser.parse
-import kotlin.jvm.JvmStatic
-import java.util.LinkedList
 import Parser.RPN
 import java.lang.Exception
 
 
-    // @JvmStatic
+
     fun main(args: Array<String>) {
         val tokens =
             lex("a = 10; i = 0; while(i < 12){i = i + 1;} a--; a = a - 10; array new List; array.add(5-2); array.add(22); array.add(4*4); array.add(3/3); arrayNew new Set; arrayNew.add(3); arrayNew.add(6+ 7 / 2); c = array.get(2); ")
